a1269f3dc8bf26a90c34ca7e3b85452bfdc694fb
Ederson Tadeu Maschio <edmaschio@gmail.com>
2017-07-25 23:26:34 -0300

    Using simple Component

---
+++ src/app/users/users.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-users',
+    template: `
+        <p>I'm in the UsersComponent</p>
+    `
+})
+export class UsersComponent {
+
+}
