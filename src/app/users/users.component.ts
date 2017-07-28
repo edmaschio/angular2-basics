35c62f45c27648a43fea3d1110c17fbd2447cc1f
Ederson Tadeu Maschio <edmaschio@gmail.com>
2017-07-28 19:37:59 -0300

    Working with directives

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -4,8 +4,14 @@ import { Component } from "@angular/core";
     selector: 'app-users',
     template: `
         <p>I'm in the UsersComponent</p>
+        <button (click)="extraInfo = true">Show Extra Info!</button>
+        <p *ngIf="extraInfo">Some extra information on UsersComponent</p>
+        <p *ngFor="let user of users">{{ user }}<p>
+        <p [ngStyle]="{'background-color': color}">This is green</p>
     `
 })
 export class UsersComponent {
-
+    extraInfo = false;
+    color = 'green';
+    users = ['Ederson', 'Juliana', 'Wagner'];
 }
